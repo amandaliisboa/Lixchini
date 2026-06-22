c15ee5d57bab03ba9a51e2b7a14da09030e59777
Amanda Lisboa <amandacecilia323@gmail.com>
2026-06-22 19:56:29 -0300

    Adding Classes Generated

---
+++ code/Level.py
@@ -0,0 +1,14 @@
+#!usr/bin/python
+#-*- coding: utf-8 -*-
+
+Class Level:
+
+    def __init__(self):
+        self.window = None
+        self.name = None
+        self.entity_list = None
+        self.entity_list = None
+
+    def run(self, ):
+        pass
+    
