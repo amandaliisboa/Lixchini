3c28938b6ccd8ba22f8cd1656d7405180ed738cd
Amanda Lisboa <amandacecilia323@gmail.com>
2026-06-23 21:46:48 -0300

    Creating menu interations Creating ABC class Creating Factory Level 1 Bg implementation = parallax

--- code/Const.py
+++ code/Const.py
@@ -1,13 +1,26 @@
 # C
 COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255, 255, 255)
+COLOR_RED = (178, 34, 34)
+COLOR_YELLOW = (201, 162, 39)
+
+# E
+ENTITY_SPEED = {
+    'sky0': 0,
+    'sky1': 1,
+    'sky2': 2,
+    'sky3': 3,
+    'sky4': 4,
+    'sky5': 5,
+    'sky6': 6,
+}
 
 # M
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
                'NEW GAME 2P - COMPETITIVE',
                'SCORE',
-               'EXITE')
+               'EXIT')
 # w
 WIN_WIDTH = 900
 WIN_HEIGHT = 550
---
+++ code/EntityFactory.py
@@ -0,0 +1,19 @@
+#!usr/bin/python
+#-*- coding: utf-8 -*-
+from code.Background import Background
+from code.Const import WIN_WIDTH
+
+
+class EntityFactory:
+
+    @staticmethod
+    def get_entity(entity_name: str, position=(0, 0)):
+        match entity_name:
+            case 'sky':
+                list_bg = []
+
+                for i in range(7):
+                    list_bg.append(Background(f'sky{i}', position=(0,0)))
+                    list_bg.append(Background(f'sky{i}', position=(WIN_WIDTH, 0,0)))
+
+                return list_bg
